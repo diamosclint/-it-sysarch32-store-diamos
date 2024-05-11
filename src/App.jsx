bfdbfb3c09d739feda7f2eef6eff6f2e045b82b0
Clint Dart Vincent Diamos <78952-22263057@SERVER2.DOMAIN>
2024-05-11 17:53:41 +0800

    stripe checkout integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import Accessories from './components/Accessories';
 import Vinyls from './components/Vinyls';
 import Footer from './components/Footer';
 import basket from './assets/basket.svg';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51PFBXCRx6tmKnG4woax6p4qyo1xn1Is2dnmEn0PSkBQBXaeqLaL0n7zXDmV0g717f4l8fb7Jf18k2edzvIH0kvPV00cAIHZPoH');
 
 function App() {
   const [isBasketOpen, setIsBasketOpen] = useState(false);
   const [basketItems, setBasketItems] = useState([]);
+  const [error, setError] = useState(null);
 
   const toggleBasket = () => {
     setIsBasketOpen(!isBasketOpen);
@@ -26,6 +30,33 @@ function App() {
 
   const closeBasket = () => {
     setIsBasketOpen(false);
+  };
+
+  const handleClick = async () => {
+    const stripe = await stripePromise;
+
+    const response = await fetch('http://localhost:4000/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+        productName: basketItems.map(item => item.name).join(', '), 
+        price: basketItems.reduce((total, item) => total + parseFloat(item.price), 0)
+      }), 
+    });
+
+    if (response.ok) {
+      const session = await response.json();
+
+      const result = await stripe.redirectToCheckout({ sessionId: session.id });
+
+      if (result.error) {
+        setError(result.error.message);
+      }
+    } else {
+      setError('Error creating checkout session');
+    }
   };
 
   return (
@@ -61,7 +92,7 @@ function App() {
                 </div>
               )}
             </div>
-            <button className='absolute bottom-20 ml-5  grid bg-dark-gray text-white py-2 px-4 rounded-full text-md hover:bg-black'>Checkout</button>
+            <button className='absolute bottom-20 ml-5  grid bg-dark-gray text-white py-2 px-4 rounded-full text-md hover:bg-black' onClick={handleClick}>Checkout</button>
             <button className="absolute top-2 right-2 bg-gray-300 px-3 py-2 rounded-full border hover:bg-slate-200 ease-in-out" onClick={closeBasket}>✕</button>
           </div>
         )}
